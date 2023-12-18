055c21363b7b708bc54aecbab5993cb39e0818b0
Leonel Rivas <lrivas@eso.org>
2023-12-18 03:57:33 -0300

    Just another test

---
+++ Server_PC/test_args.py
@@ -0,0 +1,20 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return os.getenv('MENSAJE')
+
+if __name__ == '__main__':
+    mensaje=os.getenv('MENSAJE')
+    print(mensaje)
+    app.run(host='0.0.0.0', port=8000)
+
+
+### Cargar con:
+### MENSAJE="PICOPAPERICO" gunicorn -w 4 -b 0.0.0.0:8000 app:app
+### o gunicorn -w 1 -b 0.0.0.0:8000 test_args:app -e MENSAJE="PICOPAPERICO"
+### 
+### y acceder a http://localhost:8000
